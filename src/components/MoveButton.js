611853c3a7af36c808e7b69a98ab90397d39bbe3
conrad_buys <conradbuys11@gmail.com>
2020-11-08 15:28:53 -0500

    some more battle changes. fetch calls ahoy!

--- src/components/MoveButton.js
+++ src/components/MoveButton.js
@@ -1,6 +1,6 @@
-import React, {useState, useEffect} from 'react'
+import React from 'react'
 
-function MoveButton(props){
+const MoveButton = props => {
 
     //TODO: write some logic about being able to use based on PP
     //oh sike we don't have pp
@@ -8,7 +8,7 @@ function MoveButton(props){
     //but battlecontainer will handle that
 
     return(
-    <button onClick={} move={props.move} useMove={props.useMove}>{props.move.name}</button>
+    <button move={props.move} useMove={props.useMove}>{props.move.name} key={props.move.id}</button>
     )
 }
 
