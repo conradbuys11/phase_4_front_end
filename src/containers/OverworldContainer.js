e8d39301e3f9a661264bfe0ba3fa5aac4862a1ca
NavPoint1 <foleym156@gmail.com>
2020-11-11 09:00:26 -0800

    maze work

--- src/containers/OverworldContainer.js
+++ src/containers/OverworldContainer.js
@@ -30,7 +30,7 @@ const NANITIME = 800
 const BATTLEANIMATIONDURATION = 100
 
 const BOUNDARYTHICCNESS = 20 /* MAPSIZE / 45 */
-const MOVESPEED = 1 * MAPSIZE / 300
+const MOVESPEED = 3 * MAPSIZE / 300
 const STEPTIME = 200
 const SPRITESIZE = MAPSIZE / 20
 const AGGROWIDTH = SPRITESIZE / 4
@@ -57,122 +57,278 @@ export default class OverworldContainer extends Component {
 
     // if game included more than 1 area this would be imported from area database
     obstacles = [
-        {
-            id: 1,
-            width: MAPSIZE / 45,
-            height: MAPSIZE / 4.5,
-            x: MAPSIZE / 3,
-            y: 0
-        },
-        {
-            id: 2,
-            width: MAPSIZE / 4.5,
-            height: MAPSIZE / 15,
-            x: 0,
-            y: MAPSIZE / 2.5,
-        },
-        {
-            id: 3,
-            width: MAPSIZE / 4.5,
-            height: MAPSIZE / 45,
-            x: MAPSIZE / 3,
-            y: 0
-        },
-        {
-            id: 4,
-            width: MAPSIZE / 4.5,
-            height: MAPSIZE / 15,
-            x: 0,
-            y: -MAPSIZE / 2.5,
-        },
-        {
-            id: 5,
-            width: MAPSIZE / 45,
-            height: MAPSIZE / 4.5,
-            x: -MAPSIZE / 3,
-            y: 0
-        },
-        {
-            id: 6,
-            width: MAPSIZE / 4.5,
-            height: MAPSIZE / 45,
-            x: -MAPSIZE / 3,
-            y: 0
-        },
-        {
-            id: 7,
-            width: MAPSIZE / 45,
-            height: MAPSIZE / 45,
-            x: -MAPSIZE / 5,
-            y: -MAPSIZE / 5
-        },
+        // {
+        //     id: 1,
+        //     width: MAPSIZE / 45,
+        //     height: MAPSIZE / 4.5,
+        //     x: MAPSIZE / 3,
+        //     y: 0
+        // },
+        // {
+        //     id: 2,
+        //     width: MAPSIZE / 4.5,
+        //     height: MAPSIZE / 15,
+        //     x: 0,
+        //     y: MAPSIZE / 2.5,
+        // },
+        // {
+        //     id: 3,
+        //     width: MAPSIZE / 4.5,
+        //     height: MAPSIZE / 45,
+        //     x: MAPSIZE / 3,
+        //     y: 0
+        // },
+        // {
+        //     id: 4,
+        //     width: MAPSIZE / 4.5,
+        //     height: MAPSIZE / 15,
+        //     x: 0,
+        //     y: -MAPSIZE / 2.5,
+        // },
+        // {
+        //     id: 5,
+        //     width: MAPSIZE / 45,
+        //     height: MAPSIZE / 4.5,
+        //     x: -MAPSIZE / 3,
+        //     y: 0
+        // },
+        // {
+        //     id: 6,
+        //     width: MAPSIZE / 4.5,
+        //     height: MAPSIZE / 45,
+        //     x: -MAPSIZE / 3,
+        //     y: 0
+        // },
+        // {
+        //     id: 7,
+        //     width: MAPSIZE / 45,
+        //     height: MAPSIZE / 45,
+        //     x: -MAPSIZE / 5,
+        //     y: -MAPSIZE / 5
+        // },
         {
             id: 8,
-            width: MAPSIZE * 0.9,
+            width: MAPSIZE * 0.82,
             height: MAPSIZE / 45,
-            x: -MAPSIZE * 0,
+            x: MAPSIZE * 0.1,
             y: -MAPSIZE * 0.9
         },
         {
             id: 9,
             width: MAPSIZE / 8,
             height: MAPSIZE / 5,
-            x: -MAPSIZE * 0.5,
-            y: -MAPSIZE * 0.6
+            x: -MAPSIZE * 0.9,
+            y: -MAPSIZE * 0.7
+        },
+        // maze start
+        {
+            id: 10,
+            width: MAPSIZE / 40,
+            height: MAPSIZE / 12,
+            x: MAPSIZE * 0.7,
+            y: MAPSIZE * 0.88
+        },
+        {
+            id: 11,
+            width: MAPSIZE / 6.05,
+            height: MAPSIZE / 40,
+            x: MAPSIZE * 0.56,
+            y: MAPSIZE * 0.78
+        },
+        {
+            id: 12,
+            width: MAPSIZE / 40,
+            height: MAPSIZE / 5,
+            x: MAPSIZE * 0.85,
+            y: MAPSIZE * 0.5
+        },
+        {
+            id: 13,
+            width: MAPSIZE / 40,
+            height: MAPSIZE / 8,
+            x: MAPSIZE * 0.66,
+            y: MAPSIZE * 0.15
+        },
+        {
+            id: 14,
+            width: MAPSIZE / 40,
+            height: MAPSIZE / 8,
+            x: MAPSIZE * 0.85,
+            y: MAPSIZE * -0.1
+        },
+        {
+            id: 15,
+            width: MAPSIZE / 10.5,
+            height: MAPSIZE / 40,
+            x: MAPSIZE * 0.73,
+            y: MAPSIZE * 0
+        },
+        {
+            id: 16,
+            width: MAPSIZE / 11.7,
+            height: MAPSIZE / 40,
+            x: MAPSIZE * 0.91,
+            y: MAPSIZE * -0.25
+        },
+        {
+            id: 17,
+            width: MAPSIZE / 5.12,
+            height: MAPSIZE / 40,
+            x: MAPSIZE * 0.52,
+            y: MAPSIZE * -0.16
+        },
+        {
+            id: 18,
+            width: MAPSIZE / 40,
+            height: MAPSIZE / 5,
+            x: MAPSIZE * 0.35,
+            y: MAPSIZE * 0.06
+        },
+        {
+            id: 19,
+            width: MAPSIZE / 4.66,
+            height: MAPSIZE / 40,
+            x: MAPSIZE * 0.61,
+            y: MAPSIZE * 0.5
+        },
+        {
+            id: 20,
+            width: MAPSIZE / 10.5,
+            height: MAPSIZE / 40,
+            x: MAPSIZE * 0.23,
+            y: MAPSIZE * 0.235
+        },
+        {
+            id: 21,
+            width: MAPSIZE / 40,
+            height: MAPSIZE / 4,
+            x: MAPSIZE * 0.16,
+            y: MAPSIZE * 0.51
+        },
+        // spoopy
+        {
+            id: 22,
+            width: MAPSIZE / 7.5,
+            height: MAPSIZE / 40,
+            x: MAPSIZE * 0.05,
+            y: MAPSIZE * 0.78
+        },
+        {
+            id: 23,
+            width: MAPSIZE / 40,
+            height: MAPSIZE / 6,
+            x: MAPSIZE * -0.5,
+            y: MAPSIZE * 0.8
+        },
+        {
+            id: 24,
+            width: MAPSIZE / 3,
+            height: MAPSIZE / 40,
+            x: MAPSIZE * -0.4,
+            y: MAPSIZE * 0.61
+        },
+        {
+            id: 25,
+            width: MAPSIZE / 3.1,
+            height: MAPSIZE / 40,
+            x: MAPSIZE * -0.187,
+            y: MAPSIZE * 0.4
+        },
+        {
+            id: 26,
+            width: MAPSIZE / 6.9,
+            height: MAPSIZE / 40,
+            x: MAPSIZE * -0.85,
+            y: MAPSIZE * 0.4
+        },
+        {
+            id: 27,
+            width: MAPSIZE / 40,
+            height: MAPSIZE / 10.5,
+            x: MAPSIZE * -0.485,
+            y: MAPSIZE * 0.28
+        },
+        {
+            id: 28,
+            width: MAPSIZE / 40,
+            height: MAPSIZE / 10.5,
+            x: MAPSIZE * -0.73,
+            y: MAPSIZE * 0.08
+        },
+        {
+            id: 29,
+            width: MAPSIZE / 6.5,
+            height: MAPSIZE / 40,
+            x: MAPSIZE * -0.601,
+            y: MAPSIZE * -0.04
+        },
+        {
+            id: 30,
+            width: MAPSIZE / 7.4,
+            height: MAPSIZE / 40,
+            x: MAPSIZE * -0.86,
+            y: MAPSIZE * -0.22
+        },
+        {
+            id: 31,
+            width: MAPSIZE / 40,
+            height: MAPSIZE / 3.7,
+            x: MAPSIZE * -0.75,
+            y: MAPSIZE * -0.64
         },
     ]
 
     // ideally these should load from database
     trainers = [
-        {
-            id: 1,
-            orientation: "up",
-            sprite: rocketGruntMaleUp1,
-            size: SPRITESIZE,
-            x: MAPSIZE / 2.5,
-            y: -MAPSIZE / 12.5,
-            sightWidth: AGGROWIDTH,
-            sightHeight: AGGRODISTANCE,
-        },
-        {
-            id: 2,
-            orientation: "right",
-            sprite: rocketGruntMaleRight1,
-            size: SPRITESIZE,
-            x: MAPSIZE / 2.5,
-            y: -MAPSIZE / -12.5,
-            sightWidth: AGGRODISTANCE,
-            sightHeight: AGGROWIDTH,
-        },
-        {
-            id: 3,
-            orientation: "left",
-            sprite: rocketGruntMaleLeft1,
-            size: SPRITESIZE,
-            x: MAPSIZE / -2.5,
-            y: -MAPSIZE / -12.5,
-            sightWidth: AGGRODISTANCE,
-            sightHeight: AGGROWIDTH,
-        },
-        {
-            id: 4,
-            orientation: "down",
-            sprite: rocketGruntMaleDown1,
-            size: SPRITESIZE,
-            x: MAPSIZE / -3.75,
-            y: -MAPSIZE / -12.5,
-            sightWidth: AGGROWIDTH,
-            sightHeight: AGGRODISTANCE,
-        },
+        // {
+        //     id: 1,
+        //     orientation: "up",
+        //     sprite: rocketGruntMaleUp1,
+        //     size: SPRITESIZE,
+        //     x: MAPSIZE / 2.5,
+        //     y: -MAPSIZE / 12.5,
+        //     sightWidth: AGGROWIDTH,
+        //     sightHeight: AGGRODISTANCE,
+        // },
+        // {
+        //     id: 2,
+        //     orientation: "right",
+        //     sprite: rocketGruntMaleRight1,
+        //     size: SPRITESIZE,
+        //     x: MAPSIZE / 2.5,
+        //     y: -MAPSIZE / -12.5,
+        //     sightWidth: AGGRODISTANCE,
+        //     sightHeight: AGGROWIDTH,
+        // },
+        // {
+        //     id: 3,
+        //     orientation: "left",
+        //     sprite: rocketGruntMaleLeft1,
+        //     size: SPRITESIZE,
+        //     x: MAPSIZE / -2.5,
+        //     y: -MAPSIZE / -12.5,
+        //     sightWidth: AGGRODISTANCE,
+        //     sightHeight: AGGROWIDTH,
+        // },
+        // {
+        //     id: 4,
+        //     orientation: "down",
+        //     sprite: rocketGruntMaleDown1,
+        //     size: SPRITESIZE,
+        //     x: MAPSIZE / -3.75,
+        //     y: -MAPSIZE / -12.5,
+        //     sightWidth: AGGROWIDTH,
+        //     sightHeight: AGGRODISTANCE,
+        // },
     ]
 
     // I think it makes sense to do this map 1 time here rather than every 10ms in tick()
     obstacleCollisionMap = this.obstacles.map(obstacle => {
         return {
-            minLeft: obstacle.x - obstacle.width - SPRITESIZE / 3,
-            maxLeft: obstacle.x + obstacle.width + SPRITESIZE / 3,
-            minTop: obstacle.y - obstacle.height - SPRITESIZE,
-            maxTop: obstacle.y + obstacle.height
+            minLeft: Math.floor(obstacle.x - obstacle.width - SPRITESIZE / 3),
+            maxLeft: Math.floor(obstacle.x + obstacle.width + SPRITESIZE / 3),
+            minTop: Math.floor(obstacle.y - obstacle.height - SPRITESIZE),
+            maxTop: Math.floor(obstacle.y + obstacle.height)
         }
     })
 
@@ -548,16 +704,16 @@ export default class OverworldContainer extends Component {
                 if(collisionDetected.length > 0) {
                     switch(newFacing) {
                         case "Up":
-                            newTop = collisionDetected[0].maxTop
+                            newTop = collisionDetected[collisionDetected.length - 1].maxTop
                             break;
                         case "Down":
-                            newTop = collisionDetected[0].minTop
+                            newTop = collisionDetected[collisionDetected.length - 1].minTop
                             break;
                         case "Right":
-                            newLeft = collisionDetected[0].minLeft
+                            newLeft = collisionDetected[collisionDetected.length - 1].minLeft
                             break;
                         case "Left":
-                            newLeft = collisionDetected[0].maxLeft
+                            newLeft = collisionDetected[collisionDetected.length - 1].maxLeft
                             break;
                         default:
                             break;
